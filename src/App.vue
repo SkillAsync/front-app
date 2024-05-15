3e53cb8e077b31520fec03af7628cf5e8cca1701
Dario <dariopersonal16@gmail.com>
2024-05-15 21:56:50 +0200

    Import project and implement Tailwind CSS, header, and footer component templates.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  <!-- Se coloca el RouterView para que se renderice el componente que corresponda segun la ruta-->
+  <RouterView />
+</template>
+
+<style scoped></style>
