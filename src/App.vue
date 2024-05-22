97f8fb9104afd888e28ab587bfe706b371546b87
souhailPa <souhail@paradisealacarte.com>
2024-05-22 20:11:40 +0200

    add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
-<script setup lang="ts">
+<script  setup lang="ts">
 import { RouterView } from 'vue-router'
+import { defineComponent, onMounted } from 'vue'
+import { useQuery } from '@vue/apollo-composable';
+import { gql } from 'graphql-tag';
+import { watch } from 'vue';
+
+
+
+
+const { result, loading, error } = useQuery(gql`
+   query {
+       getAllUsers{
+        data {
+            uuid
+            first_name
+          
+        }
+      }
+    }
+`);
+
+onMounted(() => {
+  console.log(result.value)
+})
+
 </script>
 <template>
   <!-- Se coloca el RouterView para que se renderice el componente que corresponda segun la ruta-->
+  
   <RouterView />
 </template>
 
